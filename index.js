c3abc72afc4ca63802ceb4b383ea609aca35e983
Light <kirill.gus@gmail.com>
2017-09-28 14:06:26 +0300

    Init commit: try videoshow package

---
+++ index.js
@@ -0,0 +1,25 @@
+const videoshow = require('videoshow')
+
+const videoOptions = {
+  fps: 25,
+  loop: 3,
+  transition: false,
+  videoBitrate: 1024,
+  videoCodec: 'libx264',
+  size: '720x1280',
+  format: 'mp4',
+  pixelFormat: 'yuv420p'
+}
+
+videoshow([input], videoOptions)
+  .save(output)
+  .on('start', function (command) {
+    console.log('ffmpeg process started:', command)
+  })
+  .on('error', function (err, stdout, stderr) {
+    console.error('Error:', err)
+    console.error('ffmpeg stderr:', stderr)
+  })
+  .on('end', function (output) {
+    console.error('Video created in:', output)
+  })
